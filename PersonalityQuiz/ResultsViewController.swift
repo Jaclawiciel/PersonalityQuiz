7bac0dfe1aabea2829b1967240aef2c3b640c0c8
Jacek Gałka <jacgalka@icloud.com>
2017-10-06 22:15:45 +0200

    Results ready

--- PersonalityQuiz/ResultsViewController.swift
+++ PersonalityQuiz/ResultsViewController.swift
@@ -13,26 +13,31 @@ class ResultsViewController: UIViewController {
     //MARK: Properties
     var responses: [Answer]!
     
+    // MARK: Outlets
+    @IBOutlet weak var resultAnswerLabel: UILabel!
+    @IBOutlet weak var resultDefinitionLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        calculatePersonalityResult()
+        navigationItem.hidesBackButton = true
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func calculatePersonalityResult() {
+        var frequencyAnswers: [AnimalType: Int] = [:]
+        let responseTypes = responses.map { $0.type }
+        for response in responseTypes {
+            frequencyAnswers[response] = (frequencyAnswers[response] ?? 0) + 1
+        }
+        let frequentAnswersSorted = frequencyAnswers.sorted { (pair1, pair2) -> Bool in
+            return pair1.value > pair2.value
+        }
+        let mostCommonAnswer = frequentAnswersSorted.first!.key
+        resultAnswerLabel.text = "You are a \(mostCommonAnswer.rawValue)!"
+        resultDefinitionLabel.text = mostCommonAnswer.definition
     }
-    */
 
+    // MARK: Segues
+    @IBAction func unwindToQuizIntroduction(segue: UIStoryboardSegue) {
+    }
 }
